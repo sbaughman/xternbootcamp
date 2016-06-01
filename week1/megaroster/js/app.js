25f4dc079d558c503bc23c66d8b778d9707e2200
Seth Baughman <sbaughman1@me.com>
2016-06-01 13:50:56 -0400

    adds basic megaroster project

---
+++ week1/megaroster/js/app.js
@@ -0,0 +1,25 @@
+$(document).foundation();
+
+var MegaRoster = {
+  init: function() {
+    this.handleFormSubmit();
+  },
+  handleFormSubmit: function() {
+    document.querySelector('form').onsubmit = function(ev) {
+      var listItem = MegaRoster.createListItem(ev.target);
+      var list = document.querySelector('#roster');
+      list.appendChild(listItem);
+      ev.target.reset();
+      ev.preventDefault();
+    };
+  },
+  createListItem: function(form) {
+    var name = form.name.value;
+    var item = document.createElement('li');
+    item.innerHTML += name;
+    return item;
+  }
+
+};
+
+MegaRoster.init();
