d9efbdd746e7217f177ef384ed37c0d2f194399a
Seth Baughman <sbaughman1@me.com>
2016-06-01 19:59:19 -0400

    refactors how li's are selected.  Adds promote/demote link.  styling

--- week1/megaroster/js/app.js
+++ week1/megaroster/js/app.js
@@ -1,6 +1,7 @@
 $(document).foundation();
 
 var MegaRoster = {
+  counter: 1,
   init: function() {
     this.handleFormSubmit();
     this.deleteItem();
@@ -17,24 +18,64 @@ var MegaRoster = {
   createListItem: function(form) {
     var name = form.name.value;
     var listItem = document.createElement('li');
+    listItem.id = this.counter;
+    var buttonDiv = this.createLinks();
+    var nameDiv = document.createElement('div');
+    nameDiv.className += "name";
+    nameDiv.innerText += name;
+    listItem.appendChild(nameDiv);
+    listItem.appendChild(buttonDiv);
+    this.counter++;
+    return listItem;
+  },
+  createLinks: function() {
+    var linkDiv = document.createElement('div');
+    linkDiv.className += 'links';
+    linkDiv.appendChild(this.createPromoteLink());
+    linkDiv.appendChild(this.createDeleteLink());
+    return linkDiv;
+  },
+  createDeleteLink: function() {
     var remove = document.createElement('a');
     remove.style.color = "red";
     remove.href = "#";
     remove.innerText = "Delete";
+    remove.id = this.counter;
     remove.onclick = function(ev){
+      ev.preventDefault();
       MegaRoster.deleteItem(ev.target);
     };
-    listItem.innerText += name;
-    listItem.appendChild(remove);
-    return listItem;
+    return remove;
   },
-  deleteItem: function(target) {
-    if (target) {
-      var li = target.parentElement;
+  createPromoteLink: function() {
+    var promote = document.createElement('a');
+    promote.href = "#";
+    promote.innerText = "Promote";
+    promote.id = this.counter;
+    promote.onclick = function(ev) {
+      ev.preventDefault();
+      MegaRoster.promoteItem(ev.target);
+    };
+    return promote;
+  },
+  deleteItem: function(item) {
+    if (item) {
+      var li = document.getElementById(item.id);
       li.parentElement.removeChild(li);
     }
+  },
+  promoteItem: function(item) {
+    if (item) {
+      var li = document.getElementById(item.id);
+      if (item.innerText === "Promote") {
+        li.style.background = "#eeffee";
+        item.innerText = "Demote";
+      } else {
+        li.style.background = "#fff";
+        item.innerText = "Promote";
+      }
+    }
   }
-
 };
 
 MegaRoster.init();
