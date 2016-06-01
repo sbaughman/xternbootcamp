653f0b95fa80c951face9790f1da6943c78c6781
Seth Baughman <sbaughman1@me.com>
2016-06-01 17:26:20 -0400

    adds delete link to remove item

--- week1/megaroster/js/app.js
+++ week1/megaroster/js/app.js
@@ -3,6 +3,7 @@ $(document).foundation();
 var MegaRoster = {
   init: function() {
     this.handleFormSubmit();
+    this.deleteItem();
   },
   handleFormSubmit: function() {
     document.querySelector('form').onsubmit = function(ev) {
@@ -15,9 +16,23 @@ var MegaRoster = {
   },
   createListItem: function(form) {
     var name = form.name.value;
-    var item = document.createElement('li');
-    item.innerHTML += name;
-    return item;
+    var listItem = document.createElement('li');
+    var remove = document.createElement('a');
+    remove.style.color = "red";
+    remove.href = "#";
+    remove.innerText = "Delete";
+    remove.onclick = function(ev){
+      MegaRoster.deleteItem(ev.target);
+    };
+    listItem.innerText += name;
+    listItem.appendChild(remove);
+    return listItem;
+  },
+  deleteItem: function(target) {
+    if (target) {
+      var li = target.parentElement;
+      li.parentElement.removeChild(li);
+    }
   }
 
 };
