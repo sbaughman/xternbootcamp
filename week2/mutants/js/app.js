f6ef096f2b3aea9863be8afa682eddd29972f3b8
Seth Baughman <sbaughman1@me.com>
2016-06-06 13:59:22 -0400

    makes basic mutantApp that loads mutants from API and displays them on the page

--- week2/mutants/js/app.js
+++ week2/mutants/js/app.js
@@ -1,1 +1,47 @@
-$(document).foundation()
+$(document).foundation();
+
+
+var mutantApp = {
+
+  init: function() {
+    this.list = $('#mutant_list');
+    this.setupTemplates();
+    this.setupEventListeners();
+  },
+
+  setupTemplates: function() {
+    this.listItem = $('.mutant.template')
+                    .removeClass('template')
+                    .removeClass('hide')
+                    .detach();
+  },
+
+  setupEventListeners: function() {
+    var doc = $(document);
+    doc.on('click', 'a[data-get-mutants="true"]', this.getAllMutants.bind(this));
+  },
+
+  getAllMutants: function(ev) {
+    ev.preventDefault();
+    $.ajax({
+      url: $(ev.currentTarget).attr('href'),
+      method: 'get',
+      success: function(data) {
+        $.each(data, function(i, mutant) {
+          this.createListItem(mutant);
+        }.bind(this));
+      }.bind(this)
+    })
+  },
+
+  createListItem: function(mutant) {
+    var mutantItem = this.listItem.clone();
+    mutantItem.find('.mutant-name').text(mutant.mutant_name);
+    mutantItem.find('.mutant-real-name').text(mutant.real_name);
+    mutantItem.find('.mutant-power').text(mutant.power);
+    this.list.append(mutantItem);
+  },
+
+};
+
+mutantApp.init();
